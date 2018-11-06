bf2e67dea967c24570f36f8db11cdb7b787e5552
sajjadhassanpour <sajjad.hassanpour@lingarogroup.com>
2018-11-06 23:48:20 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gnarl-package';
+}
